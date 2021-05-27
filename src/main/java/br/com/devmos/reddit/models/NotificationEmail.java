31e532db300cf557c14f82d91b60ff99f0a43193
Milton Oliveira Souza <miltonrubro@gmail.com>
2021-05-27 20:34:54 -0300

    Criando service de envio de email

--- src/main/java/br/com/devmos/reddit/models/NotificationEmail.java
+++ src/main/java/br/com/devmos/reddit/models/NotificationEmail.java
@@ -4,6 +4,25 @@ public class NotificationEmail {
 	
     private String subject;
     private String recipient;
-    private String body;
+	private String body;
 
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getRecipient() {
+		return recipient;
+	}
+	public void setRecipient(String recipient) {
+		this.recipient = recipient;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+    
 }
---
+++ src/main/java/br/com/devmos/reddit/service/MailService.java
@@ -0,0 +1,45 @@
+package br.com.devmos.reddit.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+import br.com.devmos.reddit.exceptions.SpringRedditException;
+import br.com.devmos.reddit.models.NotificationEmail;
+
+@Service
+public class MailService {
+	
+	private final JavaMailSender mailSender;
+	private final MailContentBuilder mailContentBuilder;
+	private static final Logger logger = LoggerFactory.getLogger(MailService.class);
+	
+	public MailService(JavaMailSender mailSender, MailContentBuilder mailContentBuilder) {
+		this.mailSender = mailSender;
+		this.mailContentBuilder = mailContentBuilder;
+	}
+	
+	void sendEmail(NotificationEmail notificationEmail) {
+		
+		MimeMessagePreparator messagePreparator = mimeMessage -> {
+			MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+			messageHelper.setFrom("springreddit@email");
+			messageHelper.setTo(notificationEmail.getRecipient());
+			messageHelper.setSubject(notificationEmail.getSubject());
+			messageHelper.setText(notificationEmail.getBody());
+		};
+		
+		try {
+			mailSender.send(messagePreparator);
+			logger.info("Activation email sent!!");
+		}catch(MailException e) {
+			logger.error("Exception occurred when sending mail", e);
+			throw new SpringRedditException("Exception occurred when sending mail to " + notificationEmail.getRecipient(), e);
+		}
+	}
+
+}
