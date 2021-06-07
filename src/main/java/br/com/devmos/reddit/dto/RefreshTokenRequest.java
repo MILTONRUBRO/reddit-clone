60069e2d98a5e32d44aab8a741bb4807385268e8
Milton Oliveira Souza <miltonrubro@gmail.com>
2021-06-07 15:37:53 -0300

    Criando token request e response authentication

---
+++ src/main/java/br/com/devmos/reddit/dto/RefreshTokenRequest.java
@@ -0,0 +1,25 @@
+package br.com.devmos.reddit.dto;
+
+public class RefreshTokenRequest {
+	
+    private String refreshToken;
+    private String username;
+    
+	public RefreshTokenRequest(String refreshToken, String username) {
+		this.refreshToken = refreshToken;
+		this.username = username;
+	}
+	public String getRefreshToken() {
+		return refreshToken;
+	}
+	public void setRefreshToken(String refreshToken) {
+		this.refreshToken = refreshToken;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+}
