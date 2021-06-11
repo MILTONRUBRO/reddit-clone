9c8cf0bfc5e09df8a1fc8cd11d9b203d9c7f0cf6
Milton Oliveira Souza <miltonrubro@gmail.com>
2021-06-10 23:35:01 -0300

    Correcao dto

--- src/main/java/br/com/devmos/reddit/dto/VoteDto.java
+++ src/main/java/br/com/devmos/reddit/dto/VoteDto.java
@@ -3,7 +3,29 @@ package br.com.devmos.reddit.dto;
 import br.com.devmos.reddit.models.VoteType;
 
 public class VoteDto {
-    private VoteType voteType;
-    private Long postId;
+	
+	private VoteType voteType;
+	private Long postId;
+
+	public VoteType getVoteType() {
+		return voteType;
+	}
+
+	public void setVoteType(VoteType voteType) {
+		this.voteType = voteType;
+	}
+
+	public Long getPostId() {
+		return postId;
+	}
+
+	public void setPostId(Long postId) {
+		this.postId = postId;
+	}
+
+	public VoteDto(VoteType voteType, Long postId) {
+		this.voteType = voteType;
+		this.postId = postId;
+	}
 
 }
