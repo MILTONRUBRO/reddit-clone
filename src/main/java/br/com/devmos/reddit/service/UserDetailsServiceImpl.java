5c07f87d2fa18e95469444716ebf0df8d8dee8f3
Milton Oliveira Souza <miltonrubro@gmail.com>
2021-06-25 15:50:31 -0300

    Add user service

---
+++ src/main/java/br/com/devmos/reddit/service/UserDetailsServiceImpl.java
@@ -0,0 +1,45 @@
+package br.com.devmos.reddit.service;
+
+import java.util.Collection;
+import java.util.Optional;
+
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.devmos.reddit.models.User;
+import br.com.devmos.reddit.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    
+	private final UserRepository userRepository;
+    
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) {
+        Optional<User> userOptional = userRepository.findByUsername(username);
+        User user = userOptional
+                .orElseThrow(() -> new UsernameNotFoundException("No user " +
+                        "Found with username : " + username));
+
+        return new org.springframework.security
+                .core.userdetails.User(user.getUserName(), user.getPassword(),
+                user.isEnabled(), true, true,
+                true, getAuthorities("USER"));
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(String role) {
+        return singletonList(new SimpleGrantedAuthority(role));
+    }
+
+}
