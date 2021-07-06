229028c422ba83dbe9a00775814dc5c27343d886
Milton Oliveira Souza <miltonrubro@gmail.com>
2021-07-06 19:43:59 -0300

    Criandoe entidade vote

--- src/main/java/br/com/devmos/reddit/models/Vote.java
+++ src/main/java/br/com/devmos/reddit/models/Vote.java
@@ -1,10 +1,58 @@
 package br.com.devmos.reddit.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
 public class Vote {
-	
-	private Long id;
-	private VoteType voteType;
-	private Post post;
-	private User user;
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long voteId;
+    private VoteType voteType;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+    
+	public Long getVoteId() {
+		return voteId;
+	}
+
+	public void setVoteId(Long voteId) {
+		this.voteId = voteId;
+	}
+
+	public VoteType getVoteType() {
+		return voteType;
+	}
+
+	public void setVoteType(VoteType voteType) {
+		this.voteType = voteType;
+	}
+
+	public Post getPost() {
+		return post;
+	}
+
+	public void setPost(Post post) {
+		this.post = post;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
 
 }
