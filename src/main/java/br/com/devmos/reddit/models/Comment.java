6c89ffc9a718ba6619a262007c8aeb736711aa6f
Milton Oliveira Souza <miltonrubro@gmail.com>
2021-07-05 21:54:48 -0300

    Criando entidade

--- src/main/java/br/com/devmos/reddit/models/Comment.java
+++ src/main/java/br/com/devmos/reddit/models/Comment.java
@@ -2,12 +2,31 @@ package br.com.devmos.reddit.models;
 
 import java.time.Instant;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+
+@Entity
 public class Comment {
 	
-	private Long id;
-	private String text;
-	private Post post;
-	private Instant createDate;
-	private User user;
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String text;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
+    private Instant createdDate;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
 
 }
