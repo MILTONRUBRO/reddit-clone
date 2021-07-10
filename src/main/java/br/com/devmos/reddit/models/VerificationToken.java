db700561947efbfb5929c7aac1c0109c3cf0e4bb
Milton Oliveira Souza <miltonrubro@gmail.com>
2021-07-10 13:22:29 -0300

    Add entity of verification token

--- src/main/java/br/com/devmos/reddit/models/VerificationToken.java
+++ src/main/java/br/com/devmos/reddit/models/VerificationToken.java
@@ -2,13 +2,21 @@ package br.com.devmos.reddit.models;
 
 import java.time.Instant;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
 public class VerificationToken {
-
+	
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
 	private Long id;
-	private String token;
-	private User user;
-	private Instant expiryDate;
-
+    private String token;
+    @OneToOne(fetch = LAZY)
+    private User user;
+    private Instant expiryDate;
 	public VerificationToken(String token, User user, Instant expiryDate) {
 		this.token = token;
 		this.user = user;
