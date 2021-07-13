73f1cd0d561e76d9827d5c9fca29939b834aa164
Milton Oliveira Souza <miltonrubro@gmail.com>
2021-07-13 19:48:35 -0300

    Add entity of refresh token

--- src/main/java/br/com/devmos/reddit/models/RefreshToken.java
+++ src/main/java/br/com/devmos/reddit/models/RefreshToken.java
@@ -2,8 +2,16 @@ package br.com.devmos.reddit.models;
 
 import java.time.Instant;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class RefreshToken {
 	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String token;
     private Instant createdDate;
